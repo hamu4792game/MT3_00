e4be377a64c3672376fb782caae4034fbcbf31b4
hamu4792game <112112847+hamu4792game@users.noreply.github.com>
2023-04-28 12:45:38 +0900

    01_00_確認課題_終了

--- MyMatrix4x4/MyMatrix4x4.cpp
+++ MyMatrix4x4/MyMatrix4x4.cpp
@@ -348,6 +348,48 @@ Matrix4x4 MyMatrix4x4::MakeAffineMatrix(const Vector3& scale, const Vector3& rot
 	return result;
 }
 
+Matrix4x4 MyMatrix4x4::MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip)
+{
+	Reset();
+	result.m[0][0] = (1.0f / aspectRatio) * (1.0f / tanf(fovY / 2.0f));
+	result.m[1][1] = 1.0f / tanf(fovY / 2.0f);
+	result.m[2][2] = farClip / (farClip - nearClip);
+	result.m[2][3] = 1.0f;
+	result.m[3][2] = (-nearClip * farClip) / (farClip - nearClip);
+
+	return result;
+}
+
+Matrix4x4 MyMatrix4x4::MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip)
+{
+	Reset();
+	result.m[0][0] = 2.0f / (right - left);
+	result.m[1][1] = 2.0f / (top - bottom);
+	result.m[2][2] = 1.0f / (farClip - nearClip);
+	result.m[3][3] = 1.0f;
+
+	result.m[3][0] = (left + right) / (left - right);
+	result.m[3][1] = (top + bottom) / (bottom - top);
+	result.m[3][2] = nearClip / (nearClip - farClip);
+	
+	return result;
+}
+
+Matrix4x4 MyMatrix4x4::MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth)
+{
+	Reset();
+	result.m[0][0] = width / 2.0f;
+	result.m[1][1] = -height / 2.0f;
+	result.m[2][2] = maxDepth - minDepth;
+	result.m[3][3] = 1.0f;
+
+	result.m[3][0] = left + (width / 2.0f);
+	result.m[3][1] = top + (height / 2.0f);
+	result.m[3][2] = minDepth;
+
+	return result;
+}
+
 void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* text) {
 	Novice::ScreenPrintf(x, y, text);
 	for (int row = 0; row < 4; row++)
--- main.cpp
+++ main.cpp
@@ -15,10 +15,9 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR /*lpCmdLine*/,
 
 	MyMatrix4x4 matrix;
 
-	Vector3 scale{ 1.2f,0.79f,-2.1f };
-	Vector3 rotate{ 0.4f,1.43f,-0.8f };
-	Vector3 translate{ 2.7f,-4.15f,1.57f };
-	Matrix4x4 worldMatrix = matrix.MakeAffineMatrix(scale, rotate, translate);
+	Matrix4x4 orthographicMatrix = matrix.MakeOrthographicMatrix(-160.0f, 160.0f, 200.0f, 300.0f, 0.0f, 1000.0f);
+	Matrix4x4 perspectiveFovMatrix = matrix.MakePerspectiveFovMatrix(0.63f, 1.33f, 0.1f, 1000.0f);
+	Matrix4x4 viewportMatrix = matrix.MakeViewportMatrix(100.0f, 200.0f, 600.0f, 300.0f, 0.0f, 1.0f);
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -43,7 +42,9 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR /*lpCmdLine*/,
 		/// ↓描画処理ここから
 		///
 
-		MatrixScreenPrintf(0, 0, worldMatrix, "worldMatrix");
+		MatrixScreenPrintf(0, 0, orthographicMatrix, "orthographicMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 5, perspectiveFovMatrix, "perspectiveFovMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 10, viewportMatrix, "viewportMatrix");
 
 
 		///
